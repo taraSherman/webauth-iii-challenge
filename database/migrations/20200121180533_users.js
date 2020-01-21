2c876ac9aad10dd6621a67698e89130ed8df7697
Tara Sherman <taradelin@gmail.com>
2020-01-21 18:16:13 -0500

    Created migration

---
+++ database/migrations/20200121180533_users.js
@@ -0,0 +1,16 @@
+exports.up = async function (knex) {
+  await knex.schema.createTable('users', users => {
+    users.increments();
+    users.string('username')
+      .notNullable()
+      .unique()
+    users.string('password')
+      .notNullable()
+    users.string('department')
+      .notNullable
+  })
+};
+
+exports.down = async function (knex) {
+  await knex.schema.dropTableIfExists('users')
+};
